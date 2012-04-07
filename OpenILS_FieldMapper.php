3438d3039b9b7051541f60f788678d025ec8fc3d
edoceo <code@edoceo.com>
2012-04-06 18:18:19 -0700

    Now we have some streamlined methods, kind of solidified the API This code is still likely to change drastically, so be careful

--- OpenILS_FieldMapper.php
+++ OpenILS_FieldMapper.php
@@ -5,10 +5,14 @@
 
 class OpenILS_FieldMapper
 {
-    private $_map_a; // ID to Name
-    private $_map_b; // Name to ID
-    private $_obj;
+    protected $_map_a; // ID to Name
+    protected $_map_b; // Name to ID
+    protected $_obj;
 
+    protected static $_idl = '/openils/conf/fm_IDL.xml';
+
+    /**
+    */
     function __construct($obj)
     {
         $this->_obj = $obj;
@@ -18,6 +22,12 @@ class OpenILS_FieldMapper
         if (!empty($obj->__c)) {
             $this->setMap($obj->__c);
         }
+    }
+    /**
+    */
+    static function setIDL($f)
+    {
+        self::$_idl = $f;
     }
     /**
     */
@@ -32,7 +42,7 @@ class OpenILS_FieldMapper
         // $map = $doc->getElementById('brt');
         // radix::dump($map);
 
-        $xml = simplexml_load_file(Radix::$root . '/etc/fm_IDL.xml');
+        $xml = simplexml_load_file(self::$_idl);
         // // $res = $xml->getDocNamespaces(true);
         // // @todo should xpath query
         foreach ($xml->class as $c) {
--- osrf.php
+++ osrf.php
@@ -4,26 +4,71 @@
     It will get an put stuff to the HTTP translator
 */
 
+/**
+    osrf class sends and recieves messages, does some field mapping too (maybe?)
+*/
+
 class osrf
 {
     const UA = 'Edoceo OpenSRF PHP Library 2012.13';
 
-    protected $_uri_base;  // Base of Evergreen Server 
-    protected $_idl_file = '/openils/conf/fm_IDL.xml';
+    private static $_conf_uri;
+    private static $_conf_idl = '/openils/conf/fm_IDL.xml';
+
+    protected $_host_uri;  // Base of Evergreen Server 
+
     /**
     */
-    function __construct()
+    function __construct($opt)
     {
-        switch (func_num_args()) {
-        case 1:
-            $this->_uri_base = func_get_arg(0);
-            break;
-        }
+        if (is_string($opt)) {
+            $this->_uri_base = $opt;
+        } elseif (is_array($opt)) {
+            $this->_uri_base = $opt['host'];
+            $this->_idl_file = $opt['idl_file'];
+        };
     }
+    static function easy($api,$arg)
+    {
+        $req = new osrfMethod($api); // Booking Resource Type
+        //$obj->addParam('6d5624a252a75dcd802e779a7187b2be');
+        //$obj->addParam($brsrc->type);
+        foreach ($arg as $a) $req->addParam($a);
+        $msg = new osrfMessage($req);
+        // $msg = new osrfMessage($req);
+        // $r = $c->send($msg);
+        $c = new osrf(self::$_conf_uri);
+        if ($ret = $c->send($msg)) {
+            if (count($ret)==2) {
+                if ( ('osrfMessage' == $ret[1]->__c)
+                    && ('STATUS' == $ret[1]->__p->type)
+                    && (205 == $ret[1]->__p->payload->__p->statusCode) ) {
+                    // $ret = $res[0]->__p->payload->__p->content);
+                    $ret = self::map($ret[0]->__p->payload->__p->content);
+                }
+            }
+        }
+        // $ret = new OpenILS_FieldMapper($res[0]->__p->payload->__p->content);
+        // radix::dump($brt->toArray());
+        return $ret;
 
-    // __c is the class
-    // __p is the props
-    // @param $msg is an OpenSRF_Message
+    }
+    /**
+        get/set the default uri
+    */
+    static function idl($f=null)
+    {
+        if ($f) self::$_conf_idl = $f;
+        return self::$_conf_idl;
+    }
+    static function uri($h=null)
+    {
+        if ($h) self::$_conf_uri = $h;
+        return self::$_conf_uri;
+    }
+    /**
+        @param $msg osrfMessage
+    */
     function send($msg=null)
     {
         if (empty($msg)) {
@@ -35,7 +80,7 @@ class osrf
         curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
 
         $head = array(
-            sprintf('X-OpenSRF-service: %s',$msg->getMethod()->getService()), // Has to Match the Request Method
+            sprintf('X-OpenSRF-service: %s',$msg->getMethod()->getService()),
             // 'X-OpenSRF-xid: A',
             // 'X-OpenSRF-thread: 1192540427.567678.119254042731367',
             'X-OpenSRF-multipart: false', // Don't give a multi-part response
@@ -115,5 +160,47 @@ class osrf
         // }
         // curl_setopt($ch,CURLOPT_POSTFIELDS,array('osrf-msg'=>json_encode($data)));
         return $ch;
+    }
+    /**
+        Field Mapper Assist?
+    */
+    static function _idl2php()
+    {
+        // @todo load the file
+        // @todo serialize for php?
+    }
+    /**
+    */
+    static function map($o)
+    {
+        $xml = simplexml_load_file(self::$_conf_idl);
+
+        // @todo should xpath query
+        // $dns = $xml->getDocNamespaces(true);
+        // // radix::dump($dns);
+        // foreach ($dns as $p=>$u) {
+        //     // radix::dump($x);
+        //     $xml->registerXPathNamespace($p,$u);
+        // }
+        // $map = $xml->xpath('//class'); // [id=' . $o->__c . ']');
+        // radix::dump($map);
+
+        // Brute Force :(
+        foreach ($xml->class as $c) {
+            if ($c['id'] != $o->__c) {
+                continue;
+            }
+
+            $ret = new stdClass();
+            $idx = 0;
+            foreach ($c->fields->field as $f) {
+                $n = strval($f['name']);
+                $ret->$n = isset($o->__p[$idx]) ? $o->__p[$idx] : null;
+                $idx++;
+            }
+            return $ret;
+        }
+
+        return false;
     }
 }
